34b5b2ff5d5d0d84962b7986357ee55e77632253
Test P <npb.viet@gmail.com>
2025-07-24 09:00:14 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/MyProfile/",
+  root: ".",
+  build: {
+    outDir: "./dist",
+    emptyOutDir: true,
+  },
   plugins: [react()],
 });
