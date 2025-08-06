150c972530156a283a23cf33f87a06ec00ba4d59
Test P <npb.viet@gmail.com>
2025-08-06 16:21:47 +0700

    updateLinkSocial

--- src/data/socialLinks.jsx
+++ src/data/socialLinks.jsx
@@ -2,7 +2,10 @@ const socialLinks = [
   { icon: ["fab", "facebook-f"], url: "https://facebook.com/npbviet" },
   { icon: ["fab", "twitter"], url: "#" },
   { icon: ["fab", "github"], url: "https://github.com/npbviet" },
-  { icon: ["fab", "linkedin-in"], url: "#" },
+  {
+    icon: ["fab", "linkedin-in"],
+    url: "https://www.linkedin.com/in/vi%E1%BB%87t-nguy%E1%BB%85n-801350195/",
+  },
   { icon: ["fab", "instagram"], url: "#" },
 ];
 export default socialLinks;
